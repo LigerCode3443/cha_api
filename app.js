834fc0dbd48e49bc9aad81628131052c325833c1
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-09-01 18:20:29 +0300

    off socket.io

--- app.js
+++ app.js
@@ -35,28 +35,28 @@ const getUser = (userId) => {
   return users.find((user) => user.userId === userId);
 };
 
-io.on("connection", (socket) => {
-  console.log("socket connected");
+// io.on("connection", (socket) => {
+//   console.log("socket connected");
 
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
+//   socket.on("addUser", (userId) => {
+//     addUser(userId, socket.id);
+//     io.emit("getUsers", users);
+//   });
 
-  // socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-  //   const user = getUser(receiverId);
-  //   io.to(user.socketId).emit("getMessage", {
-  //     senderId,
-  //     text,
-  //   });
-  // });
+// socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+//   const user = getUser(receiverId);
+//   io.to(user.socketId).emit("getMessage", {
+//     senderId,
+//     text,
+//   });
+// });
 
-  socket.on("disconnect", () => {
-    console.log("socket disconnected");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("socket disconnected");
+//     removeUser(socket.id);
+//     io.emit("getUsers", users);
+//   });
+// });
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -80,7 +80,7 @@ mongoose
   .connect(process.env.DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    server.listen(3000, () => {
+    app.listen(3000, () => {
       console.log("Server is running. Use our API on port: 3000");
     });
   })
